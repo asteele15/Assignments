14c3c4def922ea59c758f63be903acfc135cea2e
April Graves <agraves@alightinc.com>
2015-10-06 13:10:26 -0400

    Graded

--- 621PythagoreanT/621PythagoreanT/Program.cs
+++ 621PythagoreanT/621PythagoreanT/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+//Grade: 10/10 Great job
 namespace PythagoreanTripleFinder
 {
 
@@ -10,11 +12,11 @@ namespace PythagoreanTripleFinder
     {
         static void Main(string[] args)
         {
-            for (int a = 1; a <= 500; a++)//establish and increment a variable
+            for (int a = 1; a <= 20; a++)//establish and increment a variable
             {
-                for (int b = 1; b <= 500; b++) //establish and increment b variable
+                for (int b = 1; b <= 20; b++) //establish and increment b variable
                 {
-                    for (int c = 1; c <= 500; c++) //establish and increment c variable
+                    for (int c = 1; c <= 20; c++) //establish and increment c variable
                     {
                         if ((a * a) + (b * b) == c * c) //test variables for pythagorean triple
                         Console.WriteLine("{0}, {1}, {2} ", a, b, c); // action(write each instance of the triples)            
