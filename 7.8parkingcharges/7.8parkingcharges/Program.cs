35c756ced06a80f3e04902607876824e56aabf1d
ASteele <steele_peace@rocketmail.com>
2015-10-07 00:25:26 -0400

    7.8 working Parking Charges! :)

--- 7.8parkingcharges/7.8parkingcharges/Program.cs
+++ 7.8parkingcharges/7.8parkingcharges/Program.cs
@@ -4,52 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ParkingGarage7.8
+namespace ParkingGarage78
 {
-
     public class ParkingGarage
     {     
-        //I honestly am at a loss at what I am doing wrong, 
-        //I think I get the general ideas of these things, and have reviewed the sytax in the book,
-        //but clearly I'm missing something
-
       public static void Main(string[] args)
-        {
-            //get number of cars to report fees for
-            Console.WriteLine("How many cars parked in the garage yesterday?");
-            int counter = Convert.ToInt16( Console.ReadLine() );  //assign counter to run for total number of cars
+        { //
+          Console.WriteLine("How many cars parked in the garage yesterday?"); //determine max number of loops
+              int cars = Convert.ToInt32( Console.ReadLine() ); //ask how many cars to run totals for
+              double total = 0;
 
-            //loop to gather how many hours each car parked for and
-            //calucate fees 
-            for ( counter )
-            {
-            int total = CalculateCharges( int fee ) ;
-            Console.WriteLine( "{0}", total; );
-            }//end for
+              for (int count = 1; count <= cars; count++) //create loop and run time
+              {
+                  Console.WriteLine("How many hours did car {0} park?", count); //gather information for first car
+                  double hours = Convert.ToDouble( Console.ReadLine() );
+                  hours = Math.Ceiling(hours);     //round hours up
 
-       public static int CalculateCharges( int fee );
-             int hours = Convert.ToInt32(Console.ReadLine() );
-             int fee = 2;
-             if ( int hours; hours <= 3; )
-             {   fee = 2;
-            else 
-            {    if ( int hours = 3; hours >= 23)
-                    fee = 10;  //max fee
-                 else
-                    fee = .5(hours - 3);  //calculation for hours above min charge of 3
-            }//end else if
+                  double charge = CalculateCharges(hours); //calculate charges using method
+                  total += charge; //add to running total
+                  Console.WriteLine("The charge for car {0} is {1}. \n The total charges for yesterday's cars is: {2}",
+                      count, charge, total); //display information
+                  Console.WriteLine("Press Enter to continue"); //guid user
+                  Console.ReadLine(); //pause for user
+              }//end for loop
+          }//main  
+
+      public static double CalculateCharges(double hours)
+    {   double charges = 0;  //declare variable
+          if ( hours <= 3 ) //less than hours = minimum charge
+              charges = 2;
+          if (hours > 3)    
+              charges = 2 + (hours - 3)/2;  //calculate charges for hours above 3
+          if (charges > 10)
+              charges = 10;                 //establish max charge
+           return charges;
     }       // end calculate charges
 
+     }//end class
 
-    public static int hours( int hours)
-{   Console.WriteLine("How many hours did the car park for?"); 
-    int hours = Convert.ToInt32(Console.ReadLine() );
-}//end hours
-      }//end main
-
-     
-
-              
-
-   }//end class
-
+}//end namespace
